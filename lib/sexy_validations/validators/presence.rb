a8a8517c9d92b8784c432fc604c86000cb2cf38e
Corin Langosch <info@netskin.com>
2010-09-21 10:55:28 +0100

    update for latest sequel_paperclip release

--- lib/sexy_validations/validators/presence.rb
+++ lib/sexy_validations/validators/presence.rb
@@ -3,7 +3,14 @@ module SexyValidations
   module Validators
     class Presence
       def self.validate(model, attribute, value, options)
-        return unless value.blank?
+        real_value = case value.class.name
+          when "Sequel::Plugins::Paperclip::Attachment"
+            value.queued_file
+          else
+            value
+        end
+
+        return unless real_value.blank?
 
         model.errors.add(attribute, "muss ausgefüllt werden")
       end
