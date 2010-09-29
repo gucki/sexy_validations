a3f999e98d208296b215d2479be8d3b4de2061d7
Corin Langosch <info@netskin.com>
2010-09-29 14:11:43 +0100

    support more list types for count validation

--- lib/sexy_validations/validators/count.rb
+++ lib/sexy_validations/validators/count.rb
@@ -10,15 +10,28 @@ module SexyValidations
             :within => options,
           }
         end
+        
+        case options[:within]
+          when Fixnum, Integer, Bignum
+            unless value.count == options[:within]
+              record.errors.add(attribute, "ungültige Anzahl Selektionen")
+            end
 
-        min = options[:within].min
-        if value.count < min
-          record.errors.add(attribute, "zu wenig Selektionen (mindestes #{min})")
-        end
+          when Array
+            unless options[:within].include?(value.count)
+              record.errors.add(attribute, "ungültige Anzahl Selektionen")
+            end
+          
+          when Range
+            min = options[:within].min
+            if value.count < min
+              record.errors.add(attribute, "zu wenig Selektionen (mindestes #{min})")
+            end
 
-        max = options[:within].max
-        if value.count > max
-          record.errors.add(attribute, "zu viele Selektionen (maximal #{max})")
+            max = options[:within].max
+            if value.count > max
+              record.errors.add(attribute, "zu viele Selektionen (maximal #{max})")
+            end
         end
       end
     end
